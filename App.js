c44ab0da9b066252e71bdf5508ad64051d12756c
teamsoftapps <softapps.io2@gmail.com>
2023-03-18 04:20:44 +0500

    splash screen created and animation added

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import LoginScreen from "./src/screens/Login/LoginScreen"
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from './src/screens/Splash/SplashScreen';
 
 const Stack = createStackNavigator();
 
@@ -18,10 +19,10 @@ function App() {
     <NavigationContainer>
       <SafeAreaProvider>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="login" component={LoginScreen} />
+          <Stack.Screen name='splash' component={SplashScreen} />
+          {/* <Stack.Screen name="login" component={LoginScreen} /> */}
         </Stack.Navigator>
       </SafeAreaProvider>
-      <LoginScreen/>
     </NavigationContainer>
   );
 }
---
+++ src/screens/Splash/SplashScreen.js
@@ -0,0 +1,38 @@
+import { ImageBackground, StyleSheet, Text, View,Image,Animated,TouchableOpacity } from 'react-native'
+import bg from "../../assets/images/login-background.png"
+import Logo from '../../assets/images/Logo.png';
+import buildings from "../../assets/images/splash_build.png"
+import truck from "../../assets/images/truck.png"
+import React, { useEffect, useState } from 'react'
+
+
+const SplashScreen = () => {
+    const leftValue = useState(new Animated.Value(-300))[0]
+
+    const moveTruck = () => {
+        Animated.timing(leftValue,{
+            toValue:0,
+            duration:2000,
+            useNativeDriver:true
+        }).start()
+    }
+    useEffect(()=>{
+        moveTruck()
+    },[])
+  return (
+    <ImageBackground className="flex-1 w-full h-full relative" source={bg}>
+          <Image className="self-center items-center  mt-20"  source={Logo} />
+          <Image resizeMode='cover' className='w-full mt-20'   source={buildings} />
+          <Animated.Image source={truck} style={{
+            marginTop:-130,
+            transform:[{translateX:leftValue}]
+            }}  />
+    </ImageBackground>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+ 
+})
